eeb34c0613b7afb75d4680e2b461efe71b2b768a
Sebastian <segurasebastian7887@gmail.com>
2019-02-22 17:03:13 -0300

    Font awesome included

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import posts from '../data/posts';
+import { CSSTransition } from 'react-transition-group';
 
 const Figure = styled.figure`
   flex-basis: calc(33.33% - 4rem);
@@ -11,7 +11,7 @@ const Figure = styled.figure`
   padding: 2rem;
   border: 1px solid var(--lightgrey);
   background: white;
-  box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.01);
+  box-shadow: 0 0 0 2.5px rgba(0, 0, 0, 0.01);
   position: relative;
 
   .grid-photo-wrap {
@@ -31,6 +31,9 @@ const Figure = styled.figure`
   }
 
   .button {
+    a {
+      border: 2px solid lighten(grey, 90%);
+    }
     border: 2px solid lighten(grey, 90%);
     background: none;
     flex-basis: 48%;
@@ -50,8 +53,38 @@ const Figure = styled.figure`
     }
   }
 
-  .comment-count {
+  .likes-heart {
+    background: url(http://f.cl.ly/items/3Y373q2Q3J3Y1j203n0m/Bitmap-3.png) center no-repeat;
+    background-size: contain;
+    font-size: 2rem;
+    padding: 1rem;
+    position: absolute;
+    color: var(--black);
+    left: 50%;
+    top: 50%;
+    pointer-events: none;
+  }
 
+  .likes {
+    padding: 1rem 4rem;
+    font-size: 2.5rem;
+    background: transparent;
+    border: 2px solid lighten(grey, 90%);
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:hover, &:focus {
+      border-color: var(--black);
+      outline: 0;
+    }
+  }
+
+  .comment-count {
+    font-size: 2.5rem;
+    
+    i {
+      vertical-align: middle;
+    }
   }
 
   .speech-bubble {
@@ -61,18 +94,28 @@ const Figure = styled.figure`
     display: inline-block;
     border-radius: 50%;
     position: relative;
+  }
 
-    &:after {
-      display: inline-block;
-      position: absolute;
-      content: '';
-      width: 0;
-      height: 0;
-      border-style: solid;
-      border-width: 0 1.25rem 1.25rem 0;
-      border-color: transparent var(--black) transparent transparent;
-      top: 30%;
-      left: 0;
+  .likes-heart {
+    opacity: 0;
+    transition: all 0.5s;
+    transform: translateX(-50%) translateY(-50%) scale(5);
+    display: block;
+    
+    &.like-enter {
+      transition: all 0.2s;
+      transform: translateX(-50%) translateY(-50%) scale(1);
+      opacity: 1;
+
+      &:active {
+        transform: translateX(-50%) translateY(-50%) scale(5);
+      }
+    }
+
+    &.like-leave {
+      &:active {
+        display: none;
+      }
     }
   }
 `;
@@ -83,16 +126,19 @@ const Photo = ({ post, i, comments }) => (
       <Link to={`/view/${post.code}`}>
         <img src={post.display_src} alt={post.caption} className='grid-photo' />
       </Link>
+
+      <CSSTransition classNames='like' timeout={500}>
+        <span key={post.likes} className='likes-heart'>{post.likes}</span>
+      </CSSTransition>
     </div>
 
     <figcaption>
       <p>{post.caption}</p>
       <div className='control-buttons'>
-        <button className='likes'>&hearts; {posts.likes}</button>
+        <button className='likes'>&hearts; {post.likes}</button>
         <Link to={`/view/${post.code}`} className='button'>
           <span className='comment-count'>
-            <span className='speech-bubble'></span>
-            {comments[post.code] ? comments[post.code].length : 0}
+            <i className='fa fa-comment' /> {comments[post.code] ? comments[post.code].length : 0}
           </span>
         </Link>
       </div>
