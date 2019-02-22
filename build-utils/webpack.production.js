7c4f6aae4f596729399d26ab319bed8bf6b4acb8
Sebastian <segurasebastian7887@gmail.com>
2019-02-21 22:55:45 -0300

    Webpack

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,44 +1,24 @@
-const path = require('path')
-const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: ['./src/index'],
-  output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/',
-  },
   plugins: [
     new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: "'production'",
+      },
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(png|gif|jpg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192, // only if size < 8192 image will fit, if not use url
-            },
-          },
-        ],
-      },
     ],
   },
-}
+  optimization: {
+    minimize: true,
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -7,20 +8,50 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const modeConfig = env => require(`./build-utils/webpack.${env}.js`);
 
 module.exports = ({ mode }) => {
-  console.log('MODE', mode);
   return webpackMerge(
     {
       mode,
+      devtool: 'source-map',
+      entry: ['./src/index.js'],
+      output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/',
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+          },
+          {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            use: 'file-loader',
+          },
+          {
+            test: /\.(png|gif|jpg)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192, // only if size < 8192 image will fit, if not use url
+                },
+              },
+            ],
+          },
+        ],
+      },
       plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new CleanWebpackPlugin(['dist']),
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({
           filename: 'index.html',
-          template: './index.html'
-        })
-      ]
+          template: './index.html',
+        }),
+      ],
     },
-    modeConfig(mode)
+    modeConfig(mode),
   );
-}
+};
