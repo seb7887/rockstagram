63e69b8bcc59c2738c5bc8cf8d11b6eeb2267957
Sebastian <segurasebastian7887@gmail.com>
2019-03-18 15:25:57 -0300

    User registration

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,11 @@
+const db = require('../db');
+const User = db().User;
+
 exports.signup = async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
-    const error = new Error('Testing Error Handler...');
-    error.status = 400;
-    throw error;
+    const user = await User.create({ name, email });
+    return res.status(201).json(user);
   } catch (err) {
     next(err);
   }
