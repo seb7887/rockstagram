494101f8ba356ca145adb1fb42f075e463a6c623
Sebastian <segurasebastian7887@gmail.com>
2019-03-25 12:58:21 -0300

    Server: Sentry report

--- server/handlers/error.js
+++ server/handlers/error.js
@@ -1,11 +1,27 @@
+const Sentry = require('@sentry/node');
 const logger = require('../logger');
+const { sentryDsn } = require('../config');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+if (!dev) {
+  Sentry.init({
+    dsn: sentryDsn,
+  });
+}
 
 const logError = err => {
   logger.error(err);
 };
 
+const reportError = err => {
+  Sentry.captureEvent(err);
+};
+
 const errorHandler = (error, req, res, next) => {
   logError(error);
+  if (!dev) reportError(error);
+
   return res.status(error.status || 500).json({
     error: {
       message: error.message || 'Something went wrong',
